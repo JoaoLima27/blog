b5b3a61f19776f41f0bb09e448c35403bab1b690
João Pedro Rodrigues <joaop9774@gmail.com>
2024-08-19 21:42:08 -0300

    commit up

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FuncionarioController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,3 +10,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+//Funcionario
+Route::get('/funcionario', [FuncionarioController::class, 'index']);
