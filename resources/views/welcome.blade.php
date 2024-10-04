7a2daab2c2866203c47eeabe31655a58970b1ed5
João Pedro Rodrigues <joaop9774@gmail.com>
2024-10-03 21:48:54 -0300

    deu ruim

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,37 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-<section id="work" class="main style3 primary">
-    <div class="content">
-        <header>
-            <h2>My Work</h2>
-            <p>Lorem ipsum dolor sit amet et sapien sed elementum egestas dolore condimentum.
-            Fusce blandit ultrices sapien, in accumsan orci rhoncus eu. Sed sodales venenatis
-            arcu, id varius justo euismod in. Curabitur egestas consectetur magna vitae.</p>
-        </header>
-
-        <!-- Gallery  -->
-            <div class="gallery">
-                <article class="from-left">
-                    <a href="images/fulls/01.jpg" class="image fit"><img src="images/thumbs/01.jpg" title="The Anonymous Red" alt="" /></a>
-                </article>
-                <article class="from-right">
-                    <a href="images/fulls/02.jpg" class="image fit"><img src="images/thumbs/02.jpg" title="Airchitecture II" alt="" /></a>
-                </article>
-                <article class="from-left">
-                    <a href="images/fulls/03.jpg" class="image fit"><img src="images/thumbs/03.jpg" title="Air Lounge" alt="" /></a>
-                </article>
-                <article class="from-right">
-                    <a href="images/fulls/04.jpg" class="image fit"><img src="images/thumbs/04.jpg" title="Carry on" alt="" /></a>
-                </article>
-                <article class="from-left">
-                    <a href="images/fulls/05.jpg" class="image fit"><img src="images/thumbs/05.jpg" title="The sparkling shell" alt="" /></a>
-                </article>
-                <article class="from-right">
-                    <a href="images/fulls/06.jpg" class="image fit"><img src="images/thumbs/06.jpg" title="Bent IX" alt="" /></a>
-                </article>
-            </div>
-
-    </div>
-</section>
+<article class="post">
+    <header>
+        <div class="title">
+            <h2><a href="#">Magna sed adipiscing</a></h2>
+            <p>Lorem ipsum dolor amet nullam consequat etiam feugiat</p>
+        </div>
+        <div class="meta">
+            <time class="published" datetime="2015-11-01">November 1, 2015</time>
+            <a href="#" class="author"><span class="name">Jane Doe</span><img src="images/avatar.jpg" alt="" /></a>
+        </div>
+    </header>
+    <span class="image featured"><img src="images/pic01.jpg" alt="" /></span>
+    <p>Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at. Phasellus sed ultricies mi non congue ullam corper. Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
+    <p>Nunc quis dui scelerisque, scelerisque urna ut, dapibus orci. Sed vitae condimentum lectus, ut imperdiet quam. Maecenas in justo ut nulla aliquam sodales vel at ligula. Sed blandit diam odio, sed fringilla lectus molestie sit amet. Praesent eu tortor viverra lorem mattis pulvinar feugiat in turpis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce ullamcorper tellus sit amet mattis dignissim. Phasellus ut metus ligula. Curabitur nec leo turpis. Ut gravida purus quis erat pretium, sed pellentesque massa elementum. Fusce vestibulum porta augue, at mattis justo. Integer sed sapien fringilla, dapibus risus id, faucibus ante. Pellentesque mattis nunc sit amet tortor pellentesque, non placerat neque viverra. </p>
+    <footer>
+        <ul class="stats">
+            <li><a href="#">General</a></li>
+            <li><a href="#" class="icon solid fa-heart">28</a></li>
+            <li><a href="#" class="icon solid fa-comment">128</a></li>
+        </ul>
+    </footer>
+</article>
 @endsection
