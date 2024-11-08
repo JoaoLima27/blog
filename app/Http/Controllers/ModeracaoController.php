4fae036c1571ef189f68e3c34860b59c8007af9c
João Pedro Rodrigues <joaop9774@gmail.com>
2024-11-07 21:36:56 -0300

    COMMIT

--- app/Http/Controllers/ModeracaoController.php
+++ app/Http/Controllers/ModeracaoController.php
@@ -8,7 +8,19 @@ use App\Models\DenunciarPostagem;
 class ModeracaoController extends Controller
 {
     public function ModeracaoDenunciaPostagem(){
-        $denunciaPostagem = DenunciaPostagem();
+        $denunciaPostagem = DenunciarPostagem::orderBy('id', 'DESC')->paginate(10);
         return view ('moderacao.denunciaPostagem', compact ('denunciaPostagem'));
+    }
+
+    public function ModeracaoDenunciaPostagemAceito($id){
+        $denunciaPostagem = DenunciaPostagem::find($id);
+        $denunciaPostagem->status = 'ACEITO';
+        $denunciaPostagem->save();
+
+        $postagem = Postagem::find($denunciaPostagem->postagem_id);
+        $postagem->status = 0;
+        $postagem->save();
+
+        return redirect()->route('ModeracaoDenunciaPostagem')->with('mensagem', 'Denúncia Aceita Com Sucesso!');
     }
 }
---
+++ resources/views/moderacao/denunciaPostagem.blade.php
@@ -0,0 +1,53 @@
+@extends('adminlte::page')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header text-center" style="font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif; font-size: 30px">{{ __('DENÚNCIAS') }}</div>
+
+             @if (session('mensagem'))
+             <br>
+            <div class="alert alert-success">
+                {{ session('mensagem') }}
+            </div>
+             @endif
+
+               <table>
+    <tr>
+      <th>ID</th>
+      <th>Postagem - Titulo</th>
+      <th>Postagem - Autor</th>
+      <th>Denunciante</th>
+      <th>Contéudo Denúnciado</th>
+      <th>Status</th>
+      <th style="text-align:center">Ações</th>
+    </tr>
+
+    @foreach ($denunciaPostagem as $value)
+
+    <tr>
+      <td>{{$value->id}}</td>
+      <td>{{$value->postagem->titulo}}</td>
+      <td>{{$value->postagem->autor->name}}</td>
+      <td>{{$value->denunciante->name}}</td>
+      <td>{{$value->conteudo}}</td>
+      <td style="display: flex; justify-content:center; align-itens:center">
+        <a href="{{ url('/ModeracaoDenunciaPostagemAceito/' . $value->id) }}" class="btn btn-primary">ACEITO</a>
+
+        <a href="{{ url('/categoria/' . $value->id . '/edit') }}" class="btn btn-warning">Editar</a>
+
+      </td>
+    </tr>
+    @endforeach
+  </table>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
